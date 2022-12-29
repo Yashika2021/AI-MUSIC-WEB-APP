f87d1ae0f4ebae61113607c2382f7352ec33831c
Yashika2021 <88836028+Yashika2021@users.noreply.github.com>
2022-12-29 19:43:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song1 = loadSound("music.mp3");
@@ -7,12 +11,30 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(600, 500);
-    canvas.position(330,70);
+    canvas = createCanvas(480, 350);
+    canvas.position(400,60);
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("model loaded");
 }
 
 function draw(){
-    image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 480, 350);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist x = " + leftWristX + "left wrist y = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right wrist x = "+ rightWristX + " right wrist y = "+ rightWristY);
+    }
 }
